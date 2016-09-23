c41f6618b6204ef0ad99954c1184e651e144abef
laurent derache <laurent.derache@gmail.com>
2016-09-23 18:32:58 +0800

    implement cart view

---
+++ src/mvc5/TheTruck.Web/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TheTruck.Web.DataContexts;
+using TheTruck.Web.Models;
+using TheTruck.Web.Services;
+
+namespace TheTruck.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private ProductDb db = new ProductDb();
+        private CartService cartService = new CartService(System.Web.HttpContext.Current.Session);
+
+        // GET: Cart
+        public ActionResult ShowCart()
+        {
+            var productIds = cartService.GetProducts();
+            List<CartViewModel> cart = new List<CartViewModel>();
+
+            Dictionary<int, int> quantities = new Dictionary<int, int>();
+
+            foreach (var id in productIds)
+            {
+                if (quantities.ContainsKey(id))
+                {
+                    quantities[id]++;
+                }
+                else
+                {
+                    quantities.Add(id, 1);
+                }
+            }
+
+            var products = db.Products.Where(p => productIds.Contains(p.Id));
+
+            // populate cart view model
+            foreach (var p in products)
+            {
+                var qty = quantities[p.Id];
+                var subTotal = qty * p.Price;
+
+                cart.Add(new CartViewModel
+                {
+                    Category = p.Category,
+                    Image = p.Image,
+                    Name = p.Name,
+                    Price = p.Price,
+                    Quantity = qty,
+                    SubTotal = subTotal
+                });
+            }
+
+            return View(cart);
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/mvc5/TheTruck.Web/Models/CartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using TheTruck.Entities;
+
+namespace TheTruck.Web.Models
+{
+    public class CartViewModel
+    {
+        public int Id { get; set; }
+        public Genre Category { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
