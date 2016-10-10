568ab48792a8afff74db40c0812e1498469f84a5
laurent derache <laurent.derache@gmail.com>
2016-10-10 19:05:06 +0800

    lower password requirements

--- src/mvc5/TheTruck.Web/App_Start/IdentityConfig.cs
+++ src/mvc5/TheTruck.Web/App_Start/IdentityConfig.cs
@@ -65,12 +65,11 @@ namespace TheTruck.Web
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
-
+                RequiredLength = 5,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
